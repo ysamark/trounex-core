6f37d1232ebbc23e0490b67c442b96cd7cd82b52
agostinhosamil <agostinhosaml832@gmail.com>
2023-08-15 09:09:13 +0100

    feat(conf): create Config::ReadConfigValue

--- Trounex/Application/Config.php
+++ Trounex/Application/Config.php
@@ -24,8 +24,8 @@ class Config {
   }
 
   public function __get (string $prop) {
-    if (is_object ($this->props)) {
-      return isset ($this->props->$prop) ? $this->props->$prop : null;
+    if (is_object ($this->props) && isset ($this->props->$prop)) {
+      return self::ReadConfigValue ($this->props->$prop);
     }
 
     return isset ($this->props [$prop]) ? $this->props [$prop] : null;
@@ -37,5 +37,34 @@ class Config {
     }
 
     return isset ($this->props [$prop]);
+  }
+
+  public static function ReadConfigValue ($configValue) {
+    if (is_string ($configValue)) {
+      $configVariableInterpolationRe = '/\\${([a-zA-Z0-9_\.\s\t\n]+)}/';
+
+      $newConfigValue = preg_replace_callback ($configVariableInterpolationRe, function ($match) {
+        $configVariableReference = trim ($match [1]);
+
+        $configVariableValue = conf ($configVariableReference);
+
+        if (is_string ($configVariableValue)) {
+          return $configVariableValue;
+        }
+
+        if (is_array ($configVariableValue)) {
+          return '[array Array]';
+        }
+
+        if (is_object ($configVariableValue)) {
+          $objectClassName = get_class ($configVariableValue);
+          return '[object '.$objectClassName.']';
+        }
+      }, $configValue);
+
+      return $newConfigValue;
+    }
+
+    return $configValue;
   }
 }
