0c8699bc747430737687eb61c395a300f8048d98
agostinhosamil <agostinhosaml832@gmail.com>
2023-09-05 13:46:48 +0100

    feat(view/viewEngine): create capsule view engine adapter

---
+++ Trounex/View/ViewEngine/CapsuleViewEngine.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Trounex\View\ViewEngine;
+
+class CapsuleViewEngine extends ViewEngine {
+  /**
+   * @method void
+   */
+  public function render () {
+    exit ('render a capsule component!!');
+  }
+}
