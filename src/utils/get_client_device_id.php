b2361ac835fc86aca892307790240e710e22d85c
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-07 18:50:05 +0100

    feat(utils): create util to get a uniqe client device id

--- src/utils/get_client_device_id.php
+++ src/utils/get_client_device_id.php
@@ -30,30 +30,18 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+use Trounex\Cookie;
 
-namespace App\Utils;
+if (!function_exists ('get_client_device_id')) {
+  function get_client_device_id () {
+    $clientDeviceId = Cookie::get ('client-device-id');
 
-if (!session_id ()) {
-  session_start ();
-}
+    if (empty ($clientDeviceId)) {
+      $clientDeviceId = uuid ();
 
-if (!function_exists ('ShutDownFunction')) {
-  function ShutDownFunction () {
-    $_SESSION ['flash'] = [];
-    $_SESSION ['_post'] = null;
-
-    $clientDeviceId = get_client_device_id ();
-
-    $temporaryUploadFilesRe = join (DIRECTORY_SEPARATOR, [
-      conf('paths.tmpUploadsPath'), "*.$clientDeviceId"
-    ]);
-
-    $temporaryUploadFiles = glob ($temporaryUploadFilesRe);
-
-    if (count ($temporaryUploadFiles) >= 2) {
-      foreach ($temporaryUploadFiles as $temporaryUploadFile) {
-        @unlink ($temporaryUploadFile);
-      }
+      Cookie::set ('client-device-id', $clientDeviceId);
     }
+
+    return $clientDeviceId;
   }
 }
